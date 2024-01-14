7a07850c5fef86d96be3cb092126d37f37a3538e
Rodny <rodny.ferrer@hotmail.com>
2024-01-14 15:56:00 -0500

    add urls back

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import pathlib
 from setuptools import setup, find_packages
 
 setup(name='CR-Tracker',
-      version='0.14.0',
+      version='0.15.0',
       description='A tool to track your TFSA Contribution Room from Questrade',
       author='RF',
       author_email='me@gmail.com',
@@ -13,7 +13,7 @@ setup(name='CR-Tracker',
       long_description_content_type='text/markdown',
       install_requires=['questrade_api', 'pandas', 'numpy'],
       classifiers=["Development Status :: 3 - Alpha"],
-      #url="https://github.com/rodnyf/CR-Tracker",
-      #project_urls={'Source Code': 'https://github.com/rodnyf/CR-Tracker'}
+      url="https://github.com/rodnyf/CR-Tracker",
+      project_urls={'Source Code': 'https://github.com/rodnyf/CR-Tracker'}
       )
 
