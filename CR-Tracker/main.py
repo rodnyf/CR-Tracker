41b7e1ae21f3c13c55b7c8b424bd6dc0220bcab4
Rodny <rodny.ferrer@hotmail.com>
2024-08-30 15:09:50 -0400

    updated main.py with Wsimple_data(email, password)
    and contribution_room_ws()
    
    and df_filtered = df_ws[df_ws.Year >= start_year]

--- CR-Tracker/main.py
+++ CR-Tracker/main.py
@@ -3,6 +3,89 @@ import pandas as pd
 import numpy as np
 from datetime import date,timedelta
 
+from wsimple.api import Wsimple
+def get_otp():
+    return input("Enter otpnumber: \n>>>")
+
+def Wsimple_data(email, password):
+    ws = Wsimple(email, password, otp_callback=get_otp)
+    df = pd.DataFrame(ws.get_activities(limit=99, type=['deposit', 'withdrawal'])['results'])
+    df_value = df.value.dropna().apply(pd.Series)
+    df1 = df[df.object == 'deposit'][
+        ['account_id', 'object', 'created_at', 'updated_at', 'accepted_at', 'status', 'value', 'instant_value']]
+
+    df1 = pd.concat([df1, df_value], axis=1)
+    df1['Year'] = pd.to_datetime(df1['created_at']).dt.year
+    df2 = df1.loc[:, ['Year', 'object', 'amount']]
+    ### Create a new Column called type
+    df2.loc[:, 'type'] = np.where(df2.object == 'deposit', 'Deposits', 'Withdrawals')
+    df3 = df2.groupby(['Year', 'type'])['amount'].sum().rename('netAmount').reset_index()
+    return df3
+
+def contribution_room_ws(df_ws,given_year=None, given_contr_room=None, open_year=2009, birth_year=1990,):
+    contr_start_year1 = contr_start_year(birth_year)
+
+    if given_year is None or given_contr_room is None:
+        given_year = None
+        given_contr_room = None
+
+    if given_year != None:
+        start_year = given_year
+        start_contr_room = given_contr_room
+    # If they inputted a valid open_year use the open year
+    elif open_year >= 2009:
+        start_year = open_year
+        start_contr_room = max_contr_room_Limit(birth_year, start_year)
+    # If the user did not input anything assume they are were born before 1990
+    ## and start year is 2009
+    else:
+        start_year = contr_start_year1
+        start_contr_room = max_contr_room_Limit(birth_year, start_year)
+
+    print(f'start_year is {start_year}')
+
+    # Raise error if start year is before 2009 or After the current year
+    if (start_year < 2009) or (start_year > date.today().year):
+        raise Exception('given year or open year is not Valid!')
+    df_filtered = df_ws[df_ws.Year >= start_year]
+
+    df2 = df_filtered.pivot_table(index='Year', columns='type', values='netAmount').reset_index()
+# If Deposits or withdrawals don't exist add it now
+    df2.loc[:, 'Deposits'] = df2.get('Deposits', 0)
+    df2.loc[:, 'Withdrawals'] = df2.get('Withdrawals', 0)
+
+    # first thing is limit the df to the year that was given
+    df3 = df2.reset_index(drop=True)
+
+    # Insert enough rows so that there is one row for every year until next year
+    # this will fix if they had no activity in a year.
+    b = pd.DataFrame({'Year': [x for x in range(start_year, date.today().year + 2)]})
+    df4 = pd.merge(left=df3, right=b, on='Year', how='outer').fillna(0).sort_values(by='Year').reset_index(drop=True)
+
+    # Add the Given Contribution Room on January 1st
+    # first make a new column with zeros
+    df4.insert(0, 'Contr_Room_Jan1', 0)
+    # Update the value in one cell
+    # TO DO need to fix this in case the given contribution room does not match the date range that you have for the dataframe
+    # Update Contr room on Jan 1 to be the given value
+    df4.loc[:, 'Contr_Room_Jan1'] = np.where(df4.Year == start_year, start_contr_room, df4.Contr_Room_Jan1)
+
+    # Add a new Column 'New Year Dollar limit"
+    df4.insert(0, 'New_Year_Dollar_Limit', df4['Year'].map(TFSA_dollar_limit_dict))
+
+    # Calculate Contr Room on Jan 1
+    for i in range(1, len(df4)):
+        df4.loc[i, 'Contr_Room_Jan1'] = df4.loc[i - 1, 'Contr_Room_Jan1'] - df4.loc[i - 1, 'Deposits'] - df4.loc[
+            i - 1, 'Withdrawals'] + df4.loc[i, 'New_Year_Dollar_Limit']
+
+    ## Calculate the Current Contribution Room
+    df4.insert(5, 'Current_Contr_Room', df4.Contr_Room_Jan1 - df4.Deposits)
+
+    ### print summary
+    current_contr_room, next_year_contr_room = print_summary(df4)
+
+    return current_contr_room, next_year_contr_room, df4
+
 def connect_questrade(token=None):
     # First try and see if a token already exists
     try:
@@ -124,7 +207,7 @@ def print_summary(df):
 
 # Add optional arguments here
 ## given is the contribution room and year from the CRA
-def contribution_room(given_year=None, given_contr_room=None, open_year=2009, birth_year=1990, token=None):
+def contribution_room(given_year=None, given_contr_room=None, open_year=2009, birth_year=1990, token=None,df_ws=None):
     contr_start_year1 = contr_start_year(birth_year)
 
     if given_year is None or given_contr_room is None:
@@ -152,8 +235,17 @@ def contribution_room(given_year=None, given_contr_room=None, open_year=2009, bi
     ### Get the data with create_df
     if start_year == 2009:
         print('WARNING: A valid year was not input as open_year or given_year. THIS WILL TAKE A VERY LONG TIME.')
-    df = create_df(start=(str(start_year) + '-01-01'), token=token)
-    df1 = df.groupby(['Year', 'type'])['netAmount'].sum().reset_index()
+    try:
+        df = create_df(start=(str(start_year) + '-01-01'), token=token)
+    except:
+        # create empty dataframe if you can't connect to questrade
+        df = pd.DataFrame()
+    ## Append the Wealthsimple data here
+    if not df_ws.empty:
+        df = pd.concat([df, df_ws], axis=0, ignore_index=True)
+
+    df_filtered = df[df.Year >= start_year]
+    df1 = df_filtered.groupby(['Year', 'type'])['netAmount'].sum().reset_index()
 
     df2 = df1.pivot_table(index='Year', columns='type', values='netAmount').reset_index()
     # If Deposits or withdrawals don't exist add it now
